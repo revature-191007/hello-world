07ac0afccedce288a0457b05e10a578484eda249
Mitch Goshorn <msgoshorn@gmail.com>
2019-10-07 14:37:19 -0400

    Added basic Java language notes

---
+++ src/com/google/WhyJava.java
@@ -0,0 +1,33 @@
+package com.google;
+
+/**
+ * 
+ * Object Oriented Programming Language
+ * High Level language
+ * Platform Independence
+ * 
+ * JRE - Java Runtime Environment
+ * JVM - Java Virtual Machine
+ * 
+ * OS <----------- JVM	<----------	Java App
+ *  * relatively performant for a higher level language
+ *  * Aims for fast time to market
+ *  
+ *  JVM, JRE, JDK
+ *  
+ *  * Java Development Kit - This is the kit needed to develop Java application.
+ *  		If you only want to run java apps, you do not need the JDK.
+ *  
+ *  * Java Virtual Machine - Is the application an OS runs, which 
+ *  will be leveraged to run Java Applications.
+ *  
+ *  * Java Runtime Environment - Is the environment provided to Java Applications
+ *  running within the JVM.
+ * 
+ *  * Market Share for Java - Java still is basically the top dog for 
+ *   web servers.
+ *
+ */
+public class WhyJava {
+
+}
